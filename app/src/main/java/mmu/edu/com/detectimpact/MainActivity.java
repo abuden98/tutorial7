ac8f0cdbc509eb3b5e7ea0e34712e89a8eae5e84
abuden98 <fikryizuddin1998@gmail.com>
2021-01-18 00:57:57 +0800

    tutorial7

---
+++ app/src/main/java/mmu/edu/com/detectimpact/MainActivity.java
@@ -0,0 +1,70 @@
+package mmu.edu.com.detectimpact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private Vibrator vibrator;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        textView=findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.cancel();
+            }
+        });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float accel_x = sensorEvent.values[0];
+        float accel_y = sensorEvent.values[1];
+        float accel_z = sensorEvent.values[2];
+        double magnitude = Math.sqrt(accel_x*accel_x + accel_y*accel_y + accel_z*accel_z);
+        textView.setText("Current magnitudes"+String.valueOf(magnitude)+":"+"Gravitys"+String.valueOf(SensorManager.GRAVITY_EARTH));
+        if(magnitude > 3*SensorManager.GRAVITY_EARTH) {
+            long[] patern = {0, 1000, 500, 2000, 500, 1000};
+            vibrator.vibrate(patern,1);
+        }
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, sensor);
+    }
+}
